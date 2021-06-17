333571841c547d7b8821010bd1b5c0e1af9a2e47
zjicmyw <zeroology@foxmail.com>
2021-06-17 17:13:47 +0800

    增加报错提醒

--- ok/future_record.py
+++ ok/future_record.py
@@ -1,10 +1,11 @@
+
 import json
 import time
 import sys
 import os
 o_path = os.getcwd()  # 返回当前工作目录
 sys.path.append(o_path)  # 添加自己指定的搜索路径
-from utils import tools,sms_send
+from utils import tools, sms_send
 from okex_sdk_api.okex import futures_api as future
 
 with open(o_path+"/json/accounts.json", 'r', encoding='UTF-8') as load_f:
@@ -14,9 +15,10 @@ with open(o_path+"/json/accounts.json", 'r', encoding='UTF-8') as load_f:
     passphrase = myokapi_info['passphrase']
 futureAPI = future.FutureAPI(api_key, seceret_key, passphrase, True)
 
-instrument = ['BTC-USD-210625','BTC-USD-210924']
-last_mail_text = ['','']
-
+instrument = ['BTC-USD-210924', 'BTC-USD-211231']
+qty = 110
+last_mail_text = ['', '']
+exception_num = 0
 
 # 记录okex期货下单 并发邮件
 
@@ -31,49 +33,49 @@ def bd(index):
         # long_avg_cost  short_avg_cost 开仓平均价
         # long_settlement_price short_settlement_price  结算基准价
         my_future = result['holding'][0]  # 我的持仓数据
-        res2 = futureAPI.get_fills(instrument[index], after='4196402419785729') # 我的订单数据
-        # if __name__ == "__main__":
-        #     pass
-           
-            # res2 = futureAPI.get_fills('BTC-USD-210326', after='4096402419785729')
-            # print(res2[0][0]['price'],res2[0][0]['created_at'],res2[0][0]['side'])
-        if len(res2[0])==0:
+        res2 = futureAPI.get_fills(
+            instrument[index], after='4196402419785729')  # 我的订单数据
+
+        # print(my_future)
+        # sys.exit(0)
+
+        if len(res2[0]) == 0:
             print(instrument[index]+'暂无持仓')
         else:
             if my_future['long_qty'] == '0' and my_future['short_qty'] == '0':
                 qty_type = 0  # 空仓
                 mail_text = '空仓，{}：{}-{}'.format(
-                    res2[0][0]['created_at'],res2[0][0]['side'],res2[0][0]['price'])
+                    res2[0][0]['created_at'], res2[0][0]['side'], res2[0][0]['price'])
                 sms_text = '记得看太阳下山'
             else:
                 if my_future['long_qty'] == '0':
-                    mail_text = '当前持仓{}空。上次动作：{}：{}-{}'.format(
-                        my_future['short_avg_cost'], res2[0][0]['created_at'],res2[0][0]['side'],res2[0][0]['price'])
+                    mail_text = '当前持仓{}空，全仓{}倍。上次动作：{}：{}-{}'.format(
+                        my_future['short_avg_cost'],int(my_future['short_avail_qty'])/qty, res2[0][0]['created_at'], res2[0][0]['side'], res2[0][0]['price'])
                     qty_type = 2  # 开空
                     sms_text = '今天乌云密布'
                 elif my_future['short_qty'] == '0':
-                    mail_text = '当前持仓{}多。上次动作：{}：{}-{}'.format(
-                        my_future['long_avg_cost'], res2[0][0]['created_at'],res2[0][0]['side'],res2[0][0]['price'])
+                    mail_text = '当前持仓{}多，全仓{}倍。上次动作：{}：{}-{}'.format(
+                        my_future['long_avg_cost'],int( my_future['long_avail_qty'])/qty,res2[0][0]['created_at'], res2[0][0]['side'], res2[0][0]['price'])
                     qty_type = 1  # 开多
                     sms_text = '今天万里晴空'
                 else:
                     mail_text = '多空双开，当前持仓{}多，{}空。上次动作：{}：{}-{}'.format(
-                        my_future['long_avg_cost'],my_future['short_avg_cost'], res2[0][0]['created_at'],res2[0][0]['side'],res2[0][0]['price'])
+                        my_future['long_avg_cost'], my_future['short_avg_cost'], res2[0][0]['created_at'], res2[0][0]['side'], res2[0][0]['price'])
                     qty_type = 4  # 多空双开
                     sms_text = '记得看月亮'
             mail_text = instrument[index]+mail_text
             tools.time_print(mail_text)
     except Exception as e:
-        print("future_record.py -bd()出現异常:",e)
+        print("future_record.py -bd()出现异常:", e)
+        exception_num = exception_num+1
+        if exception_num==5 or exception_num==20  or exception_num==50 :
+            sms_send.send_wecaht('future_record出现异常', e)
     if mail_text != '':
         if last_mail_text[index] == mail_text:
             print('持仓无变化')
         else:
             last_mail_text[index] = mail_text
-            sms_send.send_wecaht(sms_text,mail_text)
-    
-            
-            
+            sms_send.send_wecaht(sms_text, mail_text)
 
 
 '''
@@ -84,5 +86,5 @@ order_type
 '''
 
 if __name__ == "__main__":
-    for index in [0,1]:
+    for index in [0, 1]:
         bd(index)
